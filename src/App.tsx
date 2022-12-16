bc5c7becca7517ba41a8809ec3c7f82c02cfdee4
markothedev <markothedevmail@gmail.com>
2022-12-16 16:55:46 +0100

    added skills

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,42 @@
 import React from 'react';
 import GeneralLayout from './layout/GeneralLayout/GeneralLayout';
 import logo from './logo.svg';
+import GroupOfSkills from './shared/GroupOfSkills/GroupOfSkills';
 import HeroSection from './shared/HeroSection/HeroSection';
 
 function App() {
+
+  const skills = [
+    {
+      title:'Html',
+      yearsOfExp:3,
+    },
+    {
+      title:'Css',
+      yearsOfExp:3,
+    },
+    {
+      title:'Javascript',
+      yearsOfExp:3,
+    },
+    {
+      title:'React',
+      yearsOfExp:2,
+    },
+    {
+      title:'Python',
+      yearsOfExp:3,
+    },
+    {
+      title:'Django',
+      yearsOfExp:2,
+    }
+  ]
   return (
     <GeneralLayout>
 
       <HeroSection/>
+      <GroupOfSkills  skills={skills}/>
     </GeneralLayout>
   );
 }

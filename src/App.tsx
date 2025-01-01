7df09976d2d83bc3ee6440f812c2d84e79f29c62
markothedev <markothedevmail@gmail.com>
2025-01-01 09:43:41 +0100

    updated the project list and years

--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,78 @@
-import React from 'react';
-import { useMediaQuery } from 'react-responsive';
-import GeneralLayout from './layout/GeneralLayout/GeneralLayout';
+import React from "react";
+import { useMediaQuery } from "react-responsive";
+import GeneralLayout from "./layout/GeneralLayout/GeneralLayout";
 // import logo from './logo.svg';
-import Projects from './shared/Projects/Projects';
-import GroupOfSkills from './shared/GroupOfSkills/GroupOfSkills';
-import HeroSection from './shared/HeroSection/HeroSection';
-import Line from './shared/Line/Line';
-import { Toaster } from 'react-hot-toast';
+import Projects from "./shared/Projects/Projects";
+import GroupOfSkills from "./shared/GroupOfSkills/GroupOfSkills";
+import HeroSection from "./shared/HeroSection/HeroSection";
+import Line from "./shared/Line/Line";
+import { Toaster } from "react-hot-toast";
 function App() {
-
   const skills = [
     {
-      title:'Html',
-      yearsOfExp:3,
+      title: "Html",
+      yearsOfExp: 5,
     },
     {
-      title:'Css',
-      yearsOfExp:3,
+      title: "Css",
+      yearsOfExp: 5,
     },
     {
-      title:'Javascript',
-      yearsOfExp:3,
+      title: "Javascript",
+      yearsOfExp: 5,
     },
     {
-      title:'React',
-      yearsOfExp:3,
+      title: "React",
+      yearsOfExp: 5,
     },
     {
-      title:'Python',
-      yearsOfExp:3,
+      title: "React Native",
+      yearsOfExp: 5,
     },
     {
-      title:'Django',
-      yearsOfExp:3,
+      title: "Python",
+      yearsOfExp: 5,
     },
     {
-      title:'NextJS',
-      yearsOfExp:3,
+      title: "Django",
+      yearsOfExp: 5,
     },
-  ]
+    {
+      title: "NextJS",
+      yearsOfExp: 5,
+    },
+  ];
   const isTab = useMediaQuery({
-    query: '(min-width: 600px)'
-  })
+    query: "(min-width: 600px)",
+  });
 
   return (
     <GeneralLayout>
       <Toaster />
-      <HeroSection/>
-      {
-        !isTab?
-          <>
-            <br />
-            <br /> 
-          </>
-          :''
-      }
-      <Line/>
+      <HeroSection />
+      {!isTab ? (
+        <>
+          <br />
+          <br />
+        </>
+      ) : (
+        ""
+      )}
+      <Line />
       <br />
-      <GroupOfSkills  skills={skills}/>
-      {
-        isTab?'':
-          <>
-            <br />
-            <Line/>
-            <br />
-            <br />
-            
-          </>
-      
-      }
+      <GroupOfSkills skills={skills} />
+      {isTab ? (
+        ""
+      ) : (
+        <>
+          <br />
+          <Line />
+          <br />
+          <br />
+        </>
+      )}
       <br />
-      <Projects/>
+      <Projects />
     </GeneralLayout>
   );
 }
--- src/shared/Projects/Projects.tsx
+++ src/shared/Projects/Projects.tsx
@@ -1,86 +1,124 @@
-import { useMediaQuery } from 'react-responsive'
-import PortfolioDisplay, { PortfolioDisplayProp } from '../PortfoilioDisplay/PortfolioDisplay'
-import UnderLineSpan from '../UnderLineSpan/UnderLineSpan'
-import { ListOFProject, ProjectsContainer, ProjectsHeaderContainer } from './Projects.style'
-import Projectdummy from '../../assets/images/projectdummy.webp'
-import checkidImg from '../../assets/images/checkid.jpg'
-import EmetricImg  from '../../assets/images/emetricsuite.jpg'
-import meticapp from '../../assets/images/metic.jpg'
-import manna from '../../assets/images/mannaexpress.jpg'
-const Projects = ():React.ReactElement=>{
+import { useMediaQuery } from "react-responsive";
+import PortfolioDisplay, {
+  PortfolioDisplayProp,
+} from "../PortfoilioDisplay/PortfolioDisplay";
+import UnderLineSpan from "../UnderLineSpan/UnderLineSpan";
+import {
+  ListOFProject,
+  ProjectsContainer,
+  ProjectsHeaderContainer,
+} from "./Projects.style";
+import Projectdummy from "../../assets/images/projectdummy.webp";
+import idfroms from "../../assets/images/idforms.jpeg";
+import wargerwarz from "../../assets/images/wargerwarz.png";
+import checkidImg from "../../assets/images/checkid.jpg";
+import EmetricImg from "../../assets/images/emetricsuite.png";
+import meticapp from "../../assets/images/metic.jpg";
+import manna from "../../assets/images/mannaexpress.jpg";
+const Projects = (): React.ReactElement => {
   const isTab = useMediaQuery({
-    query: '(min-width: 800px)'
-  })
+    query: "(min-width: 800px)",
+  });
 
-  const porfolio_data:PortfolioDisplayProp[] =[
+  //
+  const porfolio_data: PortfolioDisplayProp[] = [
     {
-      accessVariant:isTab?'hover':'static',
-      code_url:'#',
-      website_url:'https://checkid.ng/',
-      stacks:['stitches','NEXTJS','REACT QUERY'],
-      projectName:'CHECKID',
-      projectImage:checkidImg
+      accessVariant: isTab ? "hover" : "static",
+      code_url: "#",
+      website_url: "https://wagerwarz-landingpage.vercel.app/",
+      stacks: [
+        "React Native",
+        "REACT QUERY",
+        "Windcss",
+        "HTML",
+        "CSS",
+        "Context Api",
+      ],
+      projectName: "Wager Warz",
+      projectImage: wargerwarz,
     },
     {
-      accessVariant:isTab?'hover':'static',
-      code_url:'#',
-      website_url:'http://emetricsuite.com/',
-      stacks:['styled-components.com','NEXTJS'],
-      projectName:'Emetric Suite Landing Page',
-      projectImage:EmetricImg
+      accessVariant: isTab ? "hover" : "static",
+      code_url: "#",
+      website_url: "https://identityforms.prembly.com/",
+      stacks: ["NEXTJS", "REACT QUERY", "Tailwind", "React"],
+      projectName: "Identityforms",
+      projectImage: idfroms,
+    },
+    {
+      accessVariant: isTab ? "hover" : "static",
+      code_url: "#",
+      website_url: "https://checkid.ng/",
+      stacks: ["stitches", "NEXTJS", "REACT QUERY"],
+      projectName: "CHECKID",
+      projectImage: checkidImg,
+    },
+    {
+      accessVariant: isTab ? "hover" : "static",
+      code_url: "#",
+      website_url: "http://emetricsuite.com/",
+      stacks: ["styled-components.com", "NEXTJS"],
+      projectName: "Emetric Suite Landing Page",
+      projectImage: EmetricImg,
     },
 
     {
-      accessVariant:isTab?'hover':'static',
-      code_url:'#',
-      website_url:'https://app.emetricsuite.com/',
-      stacks:['REDUX TOOLKIT','REACT','CHAKRA UI','DJANGO REST FRAMEWORK','REDIS','PostgreSQL'],
-      projectName:'Emetric Suite web app(only desktop)',
-      projectImage:meticapp
+      accessVariant: isTab ? "hover" : "static",
+      code_url: "#",
+      website_url: "https://app.emetricsuite.com/",
+      stacks: [
+        "REDUX TOOLKIT",
+        "REACT",
+        "CHAKRA UI",
+        "DJANGO REST FRAMEWORK",
+        "REDIS",
+        "PostgreSQL",
+      ],
+      projectName: "Emetric Suite web app(only desktop)",
+      projectImage: meticapp,
     },
     {
-      accessVariant:isTab?'hover':'static',
-      code_url:'#',
-      website_url:'#',
-      stacks:['NEXTJS','stitches','REACT QUERY','ZUSTAND','PostgreSQL','django-restframework','redis',],
-      projectName:'MANNA EXPRESS',
-      projectImage:manna
-    },    
-  ]
+      accessVariant: isTab ? "hover" : "static",
+      code_url: "#",
+      website_url: "#",
+      stacks: [
+        "NEXTJS",
+        "stitches",
+        "REACT QUERY",
+        "ZUSTAND",
+        "PostgreSQL",
+        "django-restframework",
+        "redis",
+      ],
+      projectName: "MANNA EXPRESS",
+      projectImage: manna,
+    },
+  ];
   return (
     <ProjectsContainer>
-
       <ProjectsHeaderContainer>
         <h2>Projects</h2>
-        <UnderLineSpan giveSpace >
+        <UnderLineSpan giveSpace>
           <p>CONTACT ME</p>
         </UnderLineSpan>
-        
       </ProjectsHeaderContainer>
-      <br /><br />
+      <br />
+      <br />
       <ListOFProject>
-        {
-          porfolio_data.map((data,index)=>(
-            <PortfolioDisplay 
-              key={index}
-              stacks={data.stacks}
-              code_url={data.code_url}
-              website_url={data.website_url}
-              accessVariant={data.accessVariant}
-              projectImage={data.projectImage}
-              projectName={data.projectName}
-            />
-          ))
-        }
-       
-        
-        
+        {porfolio_data.map((data, index) => (
+          <PortfolioDisplay
+            key={index}
+            stacks={data.stacks}
+            code_url={data.code_url}
+            website_url={data.website_url}
+            accessVariant={data.accessVariant}
+            projectImage={data.projectImage}
+            projectName={data.projectName}
+          />
+        ))}
       </ListOFProject>
-
-     
-
     </ProjectsContainer>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
